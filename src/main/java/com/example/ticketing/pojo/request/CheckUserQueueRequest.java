e3674f5cc42717a44d334013345f291a7408732f
Akhmad bakhrul ilmi <akhmad.bakhrul@bukalapak.com>
2026-01-02 16:39:49 +0700

    remove unused import

--- src/main/java/com/example/ticketing/pojo/request/CheckUserQueueRequest.java
+++ src/main/java/com/example/ticketing/pojo/request/CheckUserQueueRequest.java
@@ -1,10 +1,7 @@
 package com.example.ticketing.pojo.request;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
